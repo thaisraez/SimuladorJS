80643449dd446e7ff54a72cb877d1614551e970c
Thais Raez <thais.raez@gmail.com>
2023-09-20 20:29:56 -0500

    new

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
-
 let comprar = prompt("Escribe un producto menstrual: Tampón, Toalla Higuiénica o Copa Menstrual")
-
-while (comprar != "ESC"){
+if (comprar != "ESC"){
     switch(comprar){
         case "Toalla Higuiénica":
             alert("Tiene un costo de $20")
             break
         case "Tampón":
-            alert("Tiene un costo de $20")
+            alert("Tiene un costo de $10")
             break
         case "Copa Menstrual":
-            alert("Tiene un costo de $20")
+            alert("Tiene un costo de $40")
             break
         default:
-            alert("No tenemos este producto")
+            alert("No tenemos este producto o está mal escrito (Recuerda usar tildes y mayúsculas)")
     }
-    comprar = prompt("Escribe un producto menstrual: Tampón, Toalla Higuiénica o Copa Menstrual")
 }
+
+// while (comprar != "ESC"){
+//     switch(comprar){
+//         case "Toalla Higuiénica":
+//             alert("Tiene un costo de $20")
+//             break
+//         case "Tampón":
+//             alert("Tiene un costo de $20")
+//             break
+//         case "Copa Menstrual":
+//             alert("Tiene un costo de $20")
+//             break
+//         default:
+//             alert("No tenemos este producto")
+//     }
+//     comprar = prompt("Escribe un producto menstrual: Tampón, Toalla Higuiénica o Copa Menstrual")
+// }
