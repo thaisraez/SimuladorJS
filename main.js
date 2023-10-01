a95501e27eda9535bb0b882964d7746862b95332
Thais Raez <thais.raez@gmail.com>
2023-10-01 18:25:24 -0500

    now

--- main.js
+++ main.js
@@ -26,38 +26,3 @@ do {
     }
   }
 } while (comprar !== "salir");
-
-
-// let comprar = prompt("Escribe un producto menstrual: Tampón, Toalla Higuiénica o Copa Menstrual")
-// if (comprar != "ESC"){
-//     switch(comprar){
-//         case "Toalla Higuiénica":
-//             alert("Tiene un costo de $20")
-//             break
-//         case "Tampón":
-//             alert("Tiene un costo de $10")
-//             break
-//         case "Copa Menstrual":
-//             alert("Tiene un costo de $40")
-//             break
-//         default:
-//             alert("No tenemos este producto o está mal escrito (Recuerda usar tildes y mayúsculas)")
-//     }
-// }
-
-// while (comprar !== "ESC"){
-//     switch(comprar){
-//         case "Toalla Higuiénica":
-//             alert("Tiene un costo de $20")
-//             break
-//         case "Tampón":
-//             alert("Tiene un costo de $20")
-//             break
-//         case "Copa Menstrual":
-//             alert("Tiene un costo de $20")
-//             break
-//         default:
-//             alert("No tenemos este producto")
-//     }
-//     comprar = prompt("Escribe un producto menstrual: Tampón, Toalla Higuiénica o Copa Menstrual")
-// }
